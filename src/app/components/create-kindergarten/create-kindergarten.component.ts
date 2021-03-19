0a679b3ebe8b8f706f2b965dac0e0c39ac1aaca3
ilyas ahmed <ilyasahmed081@gmail.com>
2021-03-19 17:09:29 +0500

    image file upload functionality added in create kindergarten screen done

--- src/app/components/create-kindergarten/create-kindergarten.component.ts
+++ src/app/components/create-kindergarten/create-kindergarten.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateKindergartenComponent implements OnInit {
 
+  preview: any = "../../../../assets/img/img-upload-icon.png";
+  loading: any = "../../../../assets/img/loading.gif";
+  imageUploaded: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  readURL(event: Event): void {
+    if (event.target['files'] && event.target['files'][0]) {
+      const file = event.target['files'][0];
+
+      const reader = new FileReader();
+      reader.onload = e => this.preview = reader.result;
+
+      reader.readAsDataURL(file);
+    }
   }
 
 }
