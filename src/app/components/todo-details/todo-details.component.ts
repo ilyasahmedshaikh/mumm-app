21e547140a630e33f1ff8dcde7079a93c0d9aba6
Muhammad Awais <awaisshaikh94@gmail.com>
2021-03-23 16:36:34 +0500

    todo details api

--- src/app/components/todo-details/todo-details.component.ts
+++ src/app/components/todo-details/todo-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common'
+import { Router } from '@angular/router';
+import{ BackNavigateService } from '../../core/services/back-navigate/back-navigate.service';
+import { ConfigService } from '../../core/http/config/config.service';
+import { ApiCallService } from '../../core/http/api-call/api-call.service';
 
 @Component({
   selector: 'app-todo-details',
@@ -7,9 +12,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoDetailsComponent implements OnInit {
 
-  constructor() { }
+  backBtnState: boolean = false;
+  todo: any = {};
+  Comments: any = [];
+  Categories: any = [];
+
+  constructor(
+    private config: ConfigService,
+    private apiCallService: ApiCallService,
+    private location: Location,
+    private router : Router,
+    private backNavigateService: BackNavigateService,
+  ) {
+    this.todo = this.router.getCurrentNavigation().extras.state.data;
+  }
 
   ngOnInit(): void {
+    this.getAllCategories();
+
+    this.backNavigateService.back.subscribe(res => {
+      this.backBtnState = res;
+    });
+
+    console.log(this.todo);
+  }
+
+  toggleBack() {
+    this.backNavigateService.toggleBackState();
+  }
+
+  back() {
+    this.location.back();
+  }
+
+  getAllCategories() {
+    this.apiCallService.getAll(this.config.tables.categoriesTable).subscribe(res => {
+      // method to format firebase data in pretty form
+      this.Categories = this.apiCallService.formatDataListing(res);
+    })
+  }
+
+  getCategoryName(id) {
+    let result = this.Categories.find( ({ Id }) => Id === id );
+    return result.name;
   }
 
 }
